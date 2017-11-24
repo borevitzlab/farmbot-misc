7402b6436eda5f290067d1bc6c5b5bb45ef441b0
mdchia <u5494149@anu.edu.au>
2017-11-25 10:29:30 +1100

    add comments

--- scraper.js
+++ scraper.js
@@ -4,28 +4,35 @@ function getPhotos(i) {
   loopLengthMs = 500;
   var theButton = document.querySelectorAll("button[class='image-flipper-left fb-button']")[0]
   var thisLink = document.querySelectorAll("img[class='image-flipper-image is-loaded-true']")[0].currentSrc;
-  //var thisTime = document.querySelectorAll("div[class='image-created-at']")[0].children[1].innerHTML
-  var thisTime = thisLink.split("?")[1]
-  var addLink = '<a href="'+thisLink+'">'+thisLink+'</a><br />';
-  var addText = '"' + thisTime + '" ' + thisLink + '<br />';
-  var addText2 = 'url="' + thisLink + '"<br />' + 'output="'+ thisTime + '.jpg"<br />'
   var nodeToAdd = document.querySelectorAll("div[class='widget-wrapper photos-widget']")[0]
-  if (typeof lastLink !== 'undefined'){
+  // human-readable time
+  //var thisTime = document.querySelectorAll("div[class='image-created-at']")[0].children[1].innerHTML
+  // the GET query is a unix timestamp
+  var thisTime = thisLink.split("?")[1]
+  // different output formats
+  var addTextAsLink = '<a href="'+thisLink+'">'+thisLink+'</a><br />';
+  var addTextAsUrlList = thisLink + '<br />';
+  var addTextForCurl = 'url="' + thisLink + '"<br />' + 'output="'+ thisTime + '.jpg"<br />'
+  if (typeof lastLink !== 'undefined'){ // check if lastLink is set
     if (lastLink == thisLink){
+      // if the last URL is the same as current, try again later
       setTimeout(function() {getPhotos(i);}, loopLengthMs);
       return;
     }
   }
   if (thisLink==""){
+    // if the image link hasn't loaded yet, try again later
+    // TODO: quit after too many attempts
     setTimeout(function() {getPhotos(i);}, loopLengthMs);
     return;
   }
-  theButton.click();
+  theButton.click(); // advance to next image
   nodeToAdd.insertAdjacentHTML('beforeend', addText2);
   if (theButton.disabled && i>1){
+    // if we clicked to the last image, do one last loop
     setTimeout(function() {getPhotos(1);}, loopLengthMs);
   } else {
     setTimeout(function() {getPhotos(i-1);}, loopLengthMs);
   }
 }
-getPhotos(100)
+getPhotos(100);
